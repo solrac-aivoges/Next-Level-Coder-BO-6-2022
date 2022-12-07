594a6148dc357586e9da5866e496eaadd6cb3bbc
solrac-aivoges <Carlos.Segovia@fundacion-jala.org>
2022-12-07 00:01:30 -0400

    tarea:Dinosaur

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -2,24 +2,48 @@ import pygame
 
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, DUCKING
+from dino_runner.utils.constants import RUNNING, DUCKING, JUMPING
 
 class Dinosaur(Sprite):
     POS_X = 80
     POS_Y = 310
     POS_Y_DUCKING = 340
+    JUMP_VEL =8.5
+
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.POS_X
         self.dino_rect.y = self.POS_Y
         self.step_index = 0
+        self.dino_run = True
+        self.dino_duck = False
+        self.dino_jump = False
+        self.jump_vel = self.JUMP_VEL
+
 
     def update(self, user_input):
-        if user_input[pygame.K_DOWN]:
+        if self.dino_jump:
+            self.jump()
+        if self.dino_duck:
             self.duck()
-        else:
+        if self.dino_run:
             self.run()
+    
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_run = False
+            self.dino_duck = True
+            self.dino_jump = False
+        elif user_input[pygame.K_UP] and not self.dino_jump:
+            self.dino_run = False 
+            self.dino_duck = False
+            self.dino_jump = True
+        elif not self.dino_jump:
+            self.dino_run = True
+            self.dino_duck = False
+            self.dino_jump = False
+
+
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -43,4 +67,12 @@ class Dinosaur(Sprite):
 
 
     def jump(self):
-        pass
+        self.image = JUMPING
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+        if self.jump_vel < -self.JUMP_VEL:
+            self.dino_rect.y = self.POS_Y
+            self.dino_jump = False 
+            self.jump_vel = self.JUMP_VEL
+            
